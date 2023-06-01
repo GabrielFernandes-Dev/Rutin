4aa23224b301577bf114b8804714b82bfc5c9c94
Gabriel Gomes <ggfernandes30@gmail.com>
2023-06-01 13:48:33 -0300

    Criando banco de dados local em sqlite para persistencia de dados. Model da tabela de atividade e usuarios criada

---
+++ Models/TbModels/UsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace Models.TbModels;
+
+[Table("tbusuario")]
+public class UsuarioModel
+{
+    [PrimaryKey, AutoIncrement]
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("nome")]
+    public string Nome { get; set; }
+    [Column("sobrenome")]
+    public string Sobrenome { get; set; }
+    [Column("cpf")]
+    public string Cpf { get; set; }
+    [Column("email")]
+    public string Email { get; set; }
+    [Column("senha")]
+    public string Senha { get; set; }
+}
